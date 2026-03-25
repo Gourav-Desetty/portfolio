b69d00ea4a9009b63292ead11228053e0bf225e0
Gourav-Desetty <139993532+Gourav-Desetty@users.noreply.github.com>
2026-03-25 12:28:05 +0530

    fix: apply hydration suppression and static params

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -2,7 +2,7 @@
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity/config'
-
+export const dynamic = 'force-static';
 // Add this block below your imports
 export const generateStaticParams = () => {
   return [{ tool: [] }];
